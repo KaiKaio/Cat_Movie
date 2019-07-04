9af1aea364b95b919ed6c4788efff55ab42dd90f
KaiKai <1035759181@qq.com>
2019-07-04 11:28:03 +0800

    城市组件数据渲染

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const axiosConfig = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://39.97.33.178',
+        changeOrigin: true
+      }
+    }
+  }
+}
+
+module.exports = axiosConfig;
